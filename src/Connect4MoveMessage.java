672bfe01c9a66c55f6a2dbc984d9faee41d4d925
Lize Chen <chenlize96@gmail.com>
2019-11-14 16:20:10 -0700

    initial

--- src/Connect4MoveMessage.java
+++ src/Connect4MoveMessage.java
@@ -1,4 +1,27 @@
+import java.io.Serializable;
 
-public class Connect4MoveMessage {
+public class Connect4MoveMessage implements Serializable {
+	public static int YELLOW = 1;
+	public static int RED = 2;
+	private static final long serialVersionUID = 1L;
+	private int row;
+	private int col;
+	private int color;
+
+	public Connect4MoveMessage(int row, int col, int color){
+		
+	}
+
+	public int getRow(){
+		return 0;
+	}
+	
+	public int getColumn(){
+		return 0;
+	}
+
+	public int getColor(){
+		return 0;
+	}
 
 }
